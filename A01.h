568913c82102af32006fc6d3db53148b0f2377f6
tanish-nigam <nigamt@purdue.edu>
2025-09-11 11:59:25 -0400

    Update A01.h

--- A01.h
+++ A01.h
@@ -1,10 +1,19 @@
-typedef struct node{
-	char name;//Name of node
-	struct node *child;
-	struct node *sibiling;
-}Node;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node {
+    char name; // Name of node
+    struct node *child;
+    struct node *sibling;
+    struct node *parent;
+} Node;
 
 Node *createNode(char input);
-void printStruct(Node *parent);
-void makeHiearchy(char *filename);
-void addChild(char parent, char child);
+Node *findNode(Node *root, char name);
+int addRelationship(Node **trees, int *size, char parentChar, char childChar);
+int hasCycle(Node *node);
+void printLevel(Node *nodes[], int count);
+void printHierarchy(Node *root);
+void freeTree(Node *node);
+int comp(const void *a, const void *b);
