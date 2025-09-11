d9cd7fabc6b671ca06aaf13314ab275e24af3120
tanish-nigam <nigamt@purdue.edu>
2025-09-11 11:59:49 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,55 @@
 #include "A01.h"
 
 
-int main(int argc, char **argv){
-	if(argc != 2){ 
-		printf("Need no more than input file");
-		return EXIT_FAILURE;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+    
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    
+    Node *trees[26] = {NULL};
+    int size = 0;
+    char buffer[256];
+    int valid = 1;
+    while (fgets(buffer, sizeof(buffer), file) != NULL && valid) {
+        char parentChar = buffer[0];
+        char childChar = buffer[1];
+        if (!addRelationship(trees, &size, parentChar, childChar)) {
+            valid = 0;
+            break;
+        }
 	}
-	
-	char *f_name = argv[1]; 
-	if(f_name == NULL){
-		printf("No file detected");
-		return EXIT_FAILURE;
-	}	
-	
-		
-	return EXIT_SUCCESS; 
+
+	if (valid) {
+        for (int i = 0; i < size; i++) {
+            if (hasCycle(trees[i])) {
+                valid = 0;
+                break;
+            }
+        }
+    }
+    if (!valid) {
+        printf("INVALID\n");
+    } else {
+		qsort(trees, size, sizeof(Node), comp);
+        for (int i = 0; i < size; i++) {
+            if (i > 0) {
+				printf("\n");
+			}
+            printHierarchy(trees[i]);
+        }
+    }
+    
+    for (int i = 0; i < size; i++) {
+        freeTree(trees[i]);
+    }
+    fclose(file);
+
+    return EXIT_SUCCESS;
 }
